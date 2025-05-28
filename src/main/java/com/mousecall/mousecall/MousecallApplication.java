29f03a50172bed5c293144da81a5c2ea4f7143ea
JUNGI JEONG <junejjk@gmail.com>
2025-05-28 23:13:22 +0900

    initial setting done

---
+++ src/main/java/com/mousecall/mousecall/MousecallApplication.java
@@ -0,0 +1,13 @@
+package com.mousecall.mousecall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MousecallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MousecallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mousecall/mousecall/MousecallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mousecall.mousecall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MousecallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
